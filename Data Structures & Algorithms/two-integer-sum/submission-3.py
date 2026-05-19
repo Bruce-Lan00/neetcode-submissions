0bef4e63136f92fbccfffd7eed3e4d923f1c200e
Tianchen Lan <84954749+Bruce-Lan00@users.noreply.github.com>
2026-05-19 16:52:44 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #brute force
+        #n = len(nums)
+        # 第一层循环：指针 i 从 0 走到倒数第二个位置
+        #for i in range(n):
+            # 第二层循环：指针 j 永远从 i 的下一个位置开始，走到最后一个位置
+        #    for j in range(i + 1, n):
+                # 如果这两个位置的数相加等于目标值
+        #       if nums[i] + nums[j] == target:
+        #            return [i, j]
+        #Hashmap    
+        hashmap = {}
+        for index,value in enumerate(nums):
+            diff = target - value
+            if diff in hashmap:
+                return [hashmap[diff],index]
+            hashmap[value]=index
+        return
