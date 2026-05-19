ea7e2ce7e1531d3277240139e7cbf8316efedba2
Tianchen Lan <84954749+Bruce-Lan00@users.noreply.github.com>
2026-05-19 16:43:41 -0400

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # optional solution which require less memory usage
+        # return sorted(t) == sorted(s)
+
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for j in s:
+            if countS[j] != countT.get(j, 0):
+                return False
+        return True
