e042ecd8253841e241807d36a142110485cea547
Tianchen Lan <84954749+Bruce-Lan00@users.noreply.github.com>
2026-05-21 17:34:07 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # my solution
+        """hashmap = {}
+        for ele in strs:
+            sort = ''.join(sorted(ele))
+            if sort in hashmap:
+                 hashmap[sort].append(ele)
+            else:
+                hashmap[sort] = [ele]
+        return list(hashmap.values())"""
+
+        # count solution
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
